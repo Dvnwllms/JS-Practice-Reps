ac931619fc1f7543e279ef7a9a346365c1c496d5
Devon Williams <dvnwllms009@gmail.com>
2018-06-04 10:58:46 -0400

    MakinMoves

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-console.log('Hello World!');
+// Lecture: Variables //
+/*
+var name = 'Devon';
+console.log(name);
+
+var lastName = 'Williams';
+console.log(lastName);
+
+var age = 29;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// Lecture: Variables 2 //
+/*
+var name = 'Devon';
+var age = 29;
+
+//console.log(name + age);
+//console.log(age + age);
+
+
+var job, isMarried;
+
+//console.log(job);
+
+job = 'bawse';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+age = 'twenty nine';
+job = 'supa bawse';
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//var lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+*/
+
+// Lecture: Operators //
+var now = 2018;
+var birthYear = now - 29;
+
+birthYear = now - 29 * 2;
+// 2018 - 58 //
+// 1960 //
+
+console.log (birthYear);
+
+var ageDevon = 29;
+var ageBob = 29;
+
+ageDevon = ageBob = (3 + 5) * 4 - 3;
+//ageDevon = ageBob = 29
+//ageDevon = 29
+
+ageDevon++;
+ageBob *= 2;
+ageBob = ageBob * 2;
+
+console.log(ageDevon);
+console.log(ageBob);
+
