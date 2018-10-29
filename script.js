ac656dce872bbf07ffe353b4647fa5e70b24fd9f
Devon Williams <dvnwllms009@gmail.com>
2018-10-29 06:31:23 -0400

    code challenge 4

--- script.js
+++ script.js
@@ -463,5 +463,53 @@ var john = {
     }
 }
 
+
+var mark = {
+    fullName: 'Mark Miller',
+    bills: [124, 48, 110, 45],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++) {
+            // Determines percentage based on tipping rules //
+            var percentage;
+            var bill = this.bills[i];
+
+            if (bill < 100) {
+                percentage = .2;   
+            } else if (bill >= 50 && bill < 300) {
+                percentage = .1;
+            } else {
+                percentage = .25
+            }
+
+            // Add results to the corresponding arrays //
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+}
+
+function calcAverage(tips) {
+    var sum = 0;
+    for (var i = 0; i > tips.length; i++) {
+        sum = sum + tips[1];
+    }
+    return sum / tips.length;
+}
+
+// Do the calculations
 john.calcTips();
-console.log(john);
+mark.calcTips();
+console.log(john, mark);
+
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+if (john.average > mark.average) {
+    console.log(john.fullName + '\'s family pays higher tips, with an average of $' + john.average);
+} else if (mark.average > john.average) { 
+    console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + mark.average);
+}
